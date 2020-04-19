22e974da4febbb0496f314f178323acb4352d48a
Naina Razafindrabiby <nainarazz@gmail.com>
2020-04-19 17:29:54 +0300

    fix margin between navbar and main content

--- src/layout/index.style.jsx
+++ src/layout/index.style.jsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 // eslint-disable-next-line import/no-named-as-default
 import media from '../config/media';
 
-// eslint-disable-next-line import/prefer-default-export
 export const LayoutContainer = styled.div`
   margin: auto;
-  margin-top: ${(props) => (props.withHeroHeader ? '0' : '75px')};
+  margin-top: ${(props) => (props.withHeroHeader ? '0' : '100px')};
   max-width: 800px;
-  padding: 10px 1.2em 20px;
+  padding: 0 20px;
   min-height: ${(props) => (props.withHeroHeader ? 'calc(100vh - 522px)' : 'calc(100vh - 152px)')};
   ${media.desktop`padding: 0;`}
 `;
