7e7c1b08aa1a86856eb6c63c57f03bc0296b564d
Naina Razafindrabiby <nainarazz@gmail.com>
2020-04-23 08:20:47 +0300

    add pagination to blog list

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,21 +1,40 @@
 import React, { useState } from 'react';
 import Helmet from 'react-helmet';
 import { graphql } from 'gatsby';
+import styled from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import Layout from '../layout/index.component';
 import config from '../../data/SiteConfig';
+import theme from '../config/theme';
 import PostListing from '../components/postListing/postListing.component';
 import { SearchContainer, Search, FilterCount } from '../page-styles/blog.style';
 
+const Loader = styled.div`
+  text-align: center;
+  color: ${theme.brand.darkShade};
+`;
+
+const pageSize = 8;
 const BlogPage = ({ data }) => {
-  const edges = (data.allMdx && data.allMdx.edges) || [];
-  const [posts, setPosts] = useState(edges);
+  const allPosts = (data.allMdx && data.allMdx.edges) || [];
+  const [filteredPosts, setFilteredPosts] = useState(allPosts);
+  const [posts, setPosts] = useState(allPosts.slice(0, pageSize));
 
   const handleSearch = (e) => {
     const searchValue = e.target.value;
-    const filteredPosts = edges.filter((p) =>
+    const filtered = allPosts.filter((p) =>
       p.node.frontmatter.title.toLowerCase().includes(searchValue.toLowerCase())
     );
-    setPosts(filteredPosts);
+    setFilteredPosts(filtered);
+    setPosts(filtered.slice(0, pageSize));
+  };
+
+  const fetchNextPosts = () => {
+    setTimeout(() => {
+      const index = posts.length;
+      const fetchedPosts = filteredPosts.slice(index, index + pageSize);
+      setPosts(posts.concat(fetchedPosts));
+    }, 100);
   };
 
   return (
@@ -27,7 +46,14 @@ const BlogPage = ({ data }) => {
           <Search type="text" placeholder="type to search blog posts" onChange={handleSearch} />
           <FilterCount>{posts.length}</FilterCount>
         </SearchContainer>
-        <PostListing postEdges={posts} />
+        <InfiniteScroll
+          dataLength={filteredPosts.length}
+          next={fetchNextPosts}
+          hasMore={posts.length < filteredPosts.length}
+          loader={<Loader>loading...</Loader>}
+        >
+          <PostListing postEdges={posts} />
+        </InfiniteScroll>
       </div>
     </Layout>
   );
