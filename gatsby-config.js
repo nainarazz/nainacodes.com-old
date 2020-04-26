bd25904497daeef02f219ce46ae593bc81edb23c
Naina Razafindrabiby <nainarazz@gmail.com>
2020-04-26 19:37:20 +0300

    set pwa display to standalone

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,7 +99,7 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: 'minimal-ui',
+        display: 'standalone',
         icons: [
           {
             src: '/logos/logo-192.png',
